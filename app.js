3b47d0ca20f79a07f461d5fba376f917a674e7ed
melissay94 <melissay94@yahoo.com>
2020-02-12 15:45:27 -0800

    Can delete and edit dinosaurs

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 const express = require("express");
 const layouts = require("express-ejs-layouts");
+const methodOverride = require("method-override");
+
 const dinoRoutes = require("./routes/dinos");
+
 
 const app = express();
 app.set("view engine", "ejs");
 app.use(layouts);
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
 
 app.get("/", (req, res) => {
     res.render("home");
--- routes/dinos.js
+++ routes/dinos.js
@@ -1,11 +1,15 @@
 const express = require("express");
+const fs = require("fs");
 
 // Add router
 const router = express.Router();
 
 // Index - GET route
 router.get("/", (req, res) => {
-    res.render("dinos/index", { dinos: [] });
+    let allDinos = fs.readFileSync("./dinosaurs.json");
+    let dinoData = JSON.parse(allDinos);
+
+    res.render("dinos/index", { dinos: dinoData });
 });
 
 // New - GET route
@@ -15,19 +19,57 @@ router.get("/new", (req, res) => {
 
 // Create - POST route
 router.post("/", (req, res) => {
+    // Read Dinos // JSON parse dinos
+    let dinos = JSON.parse(fs.readFileSync("./dinosaurs.json"));
+    // Add req.body to end of dinos
+    dinos.push(req.body);
+    // JSON strigify dinos
+    let dinoString = JSON.stringify(dinos);
+    console.log(dinoString);
+    // write dinos
+    fs.writeFileSync("./dinosaurs.json", dinoString);
 
+    // TODO: redirect to show page for new dino
+    res.redirect(`/dinos/${dinos.length-1}`);
 });
 
 // Show - GET route
 router.get("/:id", (req, res) => {
-    res.render("dinos/show", { dino: { id: req.params.id } });
+    let allDinos = JSON.parse(fs.readFileSync("./dinosaurs.json"));
+    console.log(allDinos);
+    let dinoIndex = parseInt(req.params.id);
+    let currentDino = allDinos[dinoIndex];
+    currentDino.id = dinoIndex;
+    
+    res.render("dinos/show", { dino: currentDino });
 });
 
 // Edit - GET route
 router.get("/edit/:id", (req, res) => {
-    res.render("dinos/edit", { dino: { id: req.params.id } });
+    let dinos = JSON.parse(fs.readFileSync("./dinosaurs.json"));
+    let currentDino = dinos[req.params.id];
+    currentDino.id = req.params.id;
+    res.render("dinos/edit", { dino: currentDino } );
 });
 
-// Update - Put
+// Update - PUT
+router.put("/:id", (req, res) => {
+    let dinos = JSON.parse(fs.readFileSync("./dinosaurs.json"));
+    dinos[req.params.id].name = req.body.name;
+    dinos[req.params.id].type = req.body.type;
+
+    fs.writeFileSync("./dinosaurs.json", JSON.stringify(dinos));
+    res.redirect(`/dinos/${req.params.id}`);
+});
+
+// Destroy - DELETE
+router.delete("/:id", (req, res) => {
+    let dinos = JSON.parse(fs.readFileSync("./dinosaurs.json"));
+    let deadDino = dinos.splice(req.params.id, 1);
+    console.log(deadDino);
+    fs.writeFileSync("./dinosaurs.json", JSON.stringify(dinos));
+
+    res.redirect("/dinos");
+})
 
 module.exports = router;
